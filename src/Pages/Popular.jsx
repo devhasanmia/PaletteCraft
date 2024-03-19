16ea32910c9a11cfcfb67aa20d4b219ec9e48bc9
devhasanmia <dev.hasanmia@gmail.com>
2024-03-19 12:20:20 +0600

    Popular Color Page Design Done!

--- src/Pages/Popular.jsx
+++ src/Pages/Popular.jsx
@@ -1,12 +1,35 @@
+import React from 'react';
 
+const colors = [
+  { "name": "White", "color": "#FFFFFF" },
+  { "name": "Black", "color": "#000000" },
+  { "name": "Red", "color": "#FF0000" },
+  { "name": "Green", "color": "#00FF00" },
+  { "name": "Blue", "color": "#0000FF" },
+  { "name": "Yellow", "color": "#FFFF00" },
+  { "name": "Cyan", "color": "#00FFFF" },
+  { "name": "Magenta", "color": "#FF00FF" },
+  { "name": "Gray", "color": "#808080" },
+  { "name": "Orange", "color": "#FFA500" },
+  { "name": "Purple", "color": "#800080" },
+  { "name": "Pink", "color": "#FFC0CB" }
+];
 
 const Popular = () => {
-    return (
-      <div>
-        <h1>This is Popular</h1>
-      </div>
-    )
-  }
-  
-  export default Popular
-  
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {colors.map(color => (
+        <div key={color.name} className="bg-white text-center rounded-lg shadow-md p-4">
+          <strong>{color.color}</strong>
+          <div className="w-full h-20 bg-gray-200 rounded-lg" style={{ backgroundColor: color.color }} />
+          
+          <div className="text-center mt-2">
+            <h3 className="text-lg font-semibold">{color.name}</h3>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Popular;
